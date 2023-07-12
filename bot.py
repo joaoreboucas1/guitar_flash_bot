b35c3cae9cad26b0de79c69d99c13e4a6f5f3eba
João Victor Rebouças <65295550+joaoreboucas1@users.noreply.github.com>
2023-07-12 11:08:27 -0300

    Improved chord detection, but the bot fails when a key needs to be pressed

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ from time import time, sleep
 from datetime import datetime
 import os
 import numpy as np
-from pprint import PrettyPrinter as pp
 import pyautogui as pg
 import PIL.Image as Image
 import PIL.ImageOps as ImageOps
@@ -21,18 +20,11 @@ chord_blue = (1040, 836)
 chord_orange = (1134, 836)
 
 # Chord check coordinates:
-chord_green = (790, 787)
-chord_red = (864, 787)
-chord_yellow = (947, 787)
-chord_blue = (1027, 787)
-chord_orange = (1106, 787)
-
-# Chord check coordinates 2:
-chord_green_2 = (790, 790)
-chord_red_2 = (864, 790)
-chord_yellow_2 = (947, 790)
-chord_blue_2 = (1027, 790)
-chord_orange_2 = (1106, 790)
+chord_green = (790, 790)
+chord_red = (864, 790)
+chord_yellow = (947, 790)
+chord_blue = (1027, 790)
+chord_orange = (1106, 790)
 
 # Color RGB values
 green = (0, 152, 0)
@@ -93,42 +85,44 @@ def play_song():
     action_queue = []
     frame = 0
     idle_frames = 0
+    im = screen_grab()
+    bg_green = im.getpixel(chord_green)
+    bg_red = im.getpixel(chord_red)
+    bg_yellow = im.getpixel(chord_yellow)
+    bg_blue = im.getpixel(chord_blue)
+    bg_orange = im.getpixel(chord_orange)
+    print("Background colors:", bg_green, bg_red, bg_yellow, bg_blue, bg_orange)
     while idle_frames < 3600:
         frame += 1
         if len(action_queue) > 0:
             if action_queue[-1]['frame'] == frame:
-                # print("Pressing", action_queue[-1]['action'])
+                print(f"{datetime.now()}: Pressing", action_queue[-1]['action'])
                 pg.press(action_queue[-1]['action'])
+        
         im = screen_grab()
         color_green = im.getpixel(chord_green)
         color_red = im.getpixel(chord_red)
         color_yellow = im.getpixel(chord_yellow)
         color_blue = im.getpixel(chord_blue)
         color_orange = im.getpixel(chord_orange)
-        color_green_2 = im.getpixel(chord_green_2)
-        color_red_2 = im.getpixel(chord_red_2)
-        color_yellow_2 = im.getpixel(chord_yellow_2)
-        color_blue_2 = im.getpixel(chord_blue_2)
-        color_orange_2 = im.getpixel(chord_orange_2)
 
         pressed_buttons = []
-        if color_green == green or color_green_2 == green or all(val > 210 for val in color_green):
+        if color_green != bg_green:
             pressed_buttons.append('a')
-        if color_red == red or color_red_2 == red or all(val > 210 for val in color_red):
+        if color_red != bg_red:
             pressed_buttons.append('s')
-        if color_yellow == yellow or color_yellow_2 == yellow or all(val > 210 for val in color_yellow):
+        if color_yellow != bg_yellow:
             pressed_buttons.append('j')
-        if color_blue == blue or color_blue_2 == blue or all(val > 210 for val in color_blue):
+        if color_blue != bg_blue:
             pressed_buttons.append('k')
-        if color_orange == orange or color_orange_2 == orange or all(val > 210 for val in color_orange):
+        if color_orange != bg_orange:
             pressed_buttons.append('l')
-        
         
         if pressed_buttons == previous_pressed or pressed_buttons == []:
             pressed_buttons = []
             idle_frames += 1
         else:
-            print(f"Must press {pressed_buttons} in 3 frames")
+            # print(f"Must press {pressed_buttons} in 3 frames")
             action_queue.append({'action': pressed_buttons, 'frame': frame + 3})
         
         previous_pressed = pressed_buttons
@@ -136,7 +130,7 @@ def play_song():
 
 def main():
     print(f"{datetime.now()}: Starting bot!")
-    #song = 'Technical Difficulties'
+    # #song = 'Technical Difficulties'jljljksj
     song = "Monsoon"
     start_game(song)
     print(f"{datetime.now()}: Starting game! Song: {song}")
@@ -153,11 +147,12 @@ def main():
             
 
 def get_rgb_values():
-    with Image.open("img/frame_11.png") as im:
-        print(im.getpixel(next_button))
-        #print(im.getpixel((1063,947)))
+    with Image.open("frame.png") as im:
+        #print(im.getpixel(next_button))
+        print(im.getpixel((1098, 773)))
         #print(im.getpixel((1078,727)))
 
 
 if __name__=='__main__':
     main()
+    # get_rgb_values()

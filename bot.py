14e5ddc19a2d7659f0992992bec1c8d6f67454ef
João Victor Rebouças <65295550+joaoreboucas1@users.noreply.github.com>
2023-07-14 11:43:52 -0300

    Frame processing refactoring, logging screenshots

--- bot.py
+++ bot.py
@@ -1,24 +1,11 @@
-from time import time, sleep
+from time import sleep
 from datetime import datetime
-import os
-import numpy as np
 import pyautogui as pg
 import PIL.Image as Image
-import PIL.ImageOps as ImageOps
 import PIL.ImageGrab as ImageGrab
 
-# Play area coordinates
+# Mouse scroll constant
 MOUSE_SCROLL = 120
-x_pad = 0
-y_pad = 0
-
-# Chord check coordinates:
-# Worked very well on first try but these coordinates overlap with the fire that pops up when a chord is hit.
-chord_green = (758, 836)
-chord_red = (854, 836)
-chord_yellow = (947, 836)
-chord_blue = (1040, 836)
-chord_orange = (1134, 836)
 
 # Chord check coordinates:
 chord_green = (782, 790)
@@ -33,7 +20,6 @@ hold_red = (872, 750)
 hold_yellow = (947, 750)
 hold_blue = (1019, 750)
 hold_orange = (1090, 750)
-
 
 # Color RGB values
 green = (0, 152, 0)
@@ -51,15 +37,6 @@ green_hold_color = (0, 255, 0)
 next_button = (1094, 641)
 next_color = (201,9,9)
 
-def screen_grab():
-    """
-    Description: takes a screenshot and saves to a location.
-    """
-    # box = ()
-    img = ImageGrab.grab()
-    # img.save(rf"{os.getcwd()}/full_snap_{int(time())}.png", "PNG")
-    return img
-
 def click_play():
     """
     Description: clicks on the specifies (x_play, y_play) coordinates
@@ -67,7 +44,7 @@ def click_play():
     x_play = 675
     y_play = 455
     pg.click(x_play, y_play)
-    print(f"{datetime.now()}: Click at ({x_play}, {y_play})")
+    print(f"{datetime.now()}: Clicking at ({x_play}, {y_play})")
 
 def choose_song(song):
     """
@@ -98,29 +75,34 @@ def start_game(song):
     sleep(3) # Takes a while for the song list to load
     choose_song(song)
     sleep(10) # Takes a while for the song to load
-    pg.scroll(-2*MOUSE_SCROLL) # Adjusts the screen to fit play area
-    print(f"{datetime.now()}: Scroll twice down")
+    scroll_down(2)
     sleep(1)
     pg.press('a') # Presses any button to start the song
-    print(f"{datetime.now()}: Press a")
+    print(f"{datetime.now()}: Pressing 'a'")
+
 
 def play_song():
     print(f"{datetime.now()}: Starting song...")
-    previous_pressed = []
-    action_queue = [{'action': None, 'frame': -1}]
     frame = 0
     idle_frames = 0
-    im = screen_grab()
+    previous_pressed = []
+    held_buttons = []
+    action_queue = [{'action': None, 'frame': -1}]
+    holding = False
+    interval = 3
+
+    im = ImageGrab.grab()
     bg_green = im.getpixel(chord_green)
     bg_red = im.getpixel(chord_red)
     bg_yellow = im.getpixel(chord_yellow)
     bg_blue = im.getpixel(chord_blue)
     bg_orange = im.getpixel(chord_orange)
-    print("Background colors:", bg_green, bg_red, bg_yellow, bg_blue, bg_orange)
-    holding = False
-    interval = 3
-    while idle_frames < 3600:
+    print(f"{datetime.now()}: Background colors", bg_green, bg_red, bg_yellow, bg_blue, bg_orange)
+    
+    while idle_frames < 700:
         frame += 1
+
+        # Check if bot needs to perform an action
         last_action = action_queue[-1]
         if last_action['frame'] == frame:
             if last_action['action'] == 'press':
@@ -131,7 +113,7 @@ def play_song():
                 held_buttons = last_action['buttons']
                 print(f"{datetime.now()}: Holding", held_buttons)
                 for button in held_buttons:
-                    pg.keyDown(button)                    
+                    pg.keyDown(button)
             elif last_action['action'] == 'release':
                 holding = False
                 print(f"{datetime.now()}: Releasing", held_buttons)
@@ -139,79 +121,94 @@ def play_song():
                     pg.keyUp(button)
                 held_buttons = []
             else:
-                assert False, f"Unknown action {last_action['action']}"
+                assert False, f"Unhandled action {last_action['action']}"
         
-        im = screen_grab()
-        color_green = im.getpixel(chord_green)
-        color_red = im.getpixel(chord_red)
-        color_yellow = im.getpixel(chord_yellow)
-        color_blue = im.getpixel(chord_blue)
-        color_orange = im.getpixel(chord_orange)
+        # Capture the frame to read specific pixels
+        im = ImageGrab.grab()
 
-        pressed_buttons = []
-        should_hold = False
-        if color_green != bg_green:
-            pressed_buttons.append('a')
-            should_hold = im.getpixel(hold_green) == green_hold_color
-        if color_red != bg_red:
-            pressed_buttons.append('s')
-            should_hold = should_hold or (im.getpixel(hold_red) == red_hold_color)
-        if color_yellow != bg_yellow:
-            pressed_buttons.append('j')
-            should_hold = should_hold or (im.getpixel(hold_yellow) == yellow_hold_color)
-        if color_blue != bg_blue:
-            pressed_buttons.append('k')
-            should_hold = should_hold or (im.getpixel(hold_blue) == blue_hold_color)
-        if color_orange != bg_orange:
-            pressed_buttons.append('l')
-            should_hold = should_hold or (im.getpixel(hold_orange) == orange_hold_color)
-        
-        if holding:
-            if pressed_buttons == held_buttons:
-                # print(f"Must release {pressed_buttons} in 3 frames")
-                if action_queue[-1]['action'] == 'release':
-                    pass
-                action_queue.append({'action': 'release', 'buttons': None, 'frame': frame + interval})
-
-        if pressed_buttons == previous_pressed or pressed_buttons == [] or len(pressed_buttons) == 5:
-            pressed_buttons = []
-            idle_frames += 1
-        else:
-            if should_hold and not holding:
-                if action_queue[-1]['action'] == 'hold':
-                    pass
+        # Processing frame info
+        if not holding:
+            color_green = im.getpixel(chord_green)
+            color_red = im.getpixel(chord_red)
+            color_yellow = im.getpixel(chord_yellow)
+            color_blue = im.getpixel(chord_blue)
+            color_orange = im.getpixel(chord_orange)
+            buttons_to_press = []
+            should_hold = False
+            if color_green != bg_green:
+                buttons_to_press.append('a')
+                should_hold = im.getpixel(hold_green) == green_hold_color
+            if color_red != bg_red:
+                buttons_to_press.append('s')
+                should_hold = should_hold or (im.getpixel(hold_red) == red_hold_color)
+            if color_yellow != bg_yellow:
+                buttons_to_press.append('j')
+                should_hold = should_hold or (im.getpixel(hold_yellow) == yellow_hold_color)
+            if color_blue != bg_blue:
+                buttons_to_press.append('k')
+                should_hold = should_hold or (im.getpixel(hold_blue) == blue_hold_color)
+            if color_orange != bg_orange:
+                buttons_to_press.append('l')
+                should_hold = should_hold or (im.getpixel(hold_orange) == orange_hold_color)
+            
+            if buttons_to_press == [] or len(buttons_to_press) == 5:
+                idle_frames += 1
+                continue
+            if not (action_queue[-1]['action'] == 'hold'):
+                if should_hold:
+                    idle_frames = 0
+                    action_queue.append({'action': 'hold', 'buttons': buttons_to_press, 'frame': frame + interval})
                 else:
-                    print(f"Must hold {pressed_buttons} in 3 frames")
-                    action_queue.append({'action': 'hold', 'buttons': pressed_buttons, 'frame': frame + interval})
-            else: 
-                # print(f"Must press {pressed_buttons} in 3 frames")
-                if not holding:
-                    action_queue.append({'action': 'press', 'buttons': pressed_buttons, 'frame': frame + interval})
-        
-        previous_pressed = pressed_buttons
+                    idle_frames = 0
+                    action_queue.append({'action': 'press', 'buttons': buttons_to_press, 'frame': frame + interval})
+        else:
+            color_green = im.getpixel(hold_green)
+            color_red = im.getpixel(hold_red)
+            color_yellow = im.getpixel(hold_yellow)
+            color_blue = im.getpixel(hold_blue)
+            color_orange = im.getpixel(hold_orange)
+            should_release = (
+                ('a' in held_buttons and color_green != green_hold_color) or
+                ('s' in held_buttons and color_red != red_hold_color) or
+                ('j' in held_buttons and color_yellow != yellow_hold_color) or
+                ('k' in held_buttons and color_blue != blue_hold_color) or 
+                ('l' in held_buttons and color_orange != orange_hold_color)
+            )
+            if should_release and not (action_queue[-1]['action'] == 'release'):
+                action_queue.append({'action': 'release', 'buttons': None, 'frame': frame + interval})        
+    im = ImageGrab.grab()
+    im_name = f'./statistics/{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.png'
+    im.save(im_name, "PNG")
+    print(f"{datetime.now()}: Song finished! Saved statistics screenshot in {im_name}.")
 
 
 def main():
     print(f"{datetime.now()}: Starting bot!")
-    # song = 'Technical Difficulties'
     song = "Monsoon"
+    # song = "Technical Difficulties"
     # song = "My Will Be Done"
-    song = "Breakthrough"
+    # song = "Breakthrough"
     start_game(song)
     print(f"{datetime.now()}: Starting game! Song: {song}")
     sleep(3) # Waiting 3 seconds to start song
     for _ in range(10):
         play_song()
-        print(f"{datetime.now()}: Song finished! Starting next...")
+        print(f"{datetime.now()}: Starting next song...")
         pg.click(next_button)
-        sleep(20)
-        pg.scroll(-2*MOUSE_SCROLL) # Adjusts the screen to fit play area
+        sleep(12)
+        scroll_down(2)
         sleep(1)
         pg.press('a')
+        print(f"{datetime.now()}: Pressing 'a'")
+
         
         
 def scroll_down(num):
     pg.scroll(-num*MOUSE_SCROLL)
+    if num < 0:
+        print(f"{datetime.now()}: Scrolling up {-num} times")
+    else:
+        print(f"{datetime.now()}: Scrolling down {num} times")
 
 def get_rgb_values():
     with Image.open("frame.png") as im:
@@ -220,7 +217,6 @@ def get_rgb_values():
         print(im.getpixel(hold_blue))
         print(im.getpixel(hold_red))
         #print(im.getpixel((1078,727)))
-
 
 if __name__=='__main__':
     main()

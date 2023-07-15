9bf156d82d19c608aadf2a7c9bb57630333ed5a2
João Victor Rebouças <65295550+joaoreboucas1@users.noreply.github.com>
2023-07-15 16:29:56 -0300

    Multithreading is working!

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, time
 from datetime import datetime
 import threading
 import queue
@@ -53,11 +53,11 @@ def choose_song(song):
     Description: chooses a song from the Guitar Flash list
     """
     x_song = 677
-    if song == "Monsoon":
+    if song == "The Lead Sprinkler":
         y_song = 627
-    elif song == "Technical Difficulties":
+    elif song == "Monsoon":
         y_song = 684
-    elif song == "My Will Be Done":
+    elif song == "Technical Difficulties":
         y_song = 742
     elif song == "Helicopter":
         y_song = 710
@@ -90,11 +90,12 @@ def start_game(song):
 def play_song():
     print(f"{datetime.now()}: Starting song...")
     frame = 0
-    idle_frames = 0
-    previous_pressed = []
+    start_time = time()
     held_buttons = []
     holding = False
-    interval = 3
+    interval = 0.2
+    scheduled_hold = False
+    scheduled_release = False
 
     im = ImageGrab.grab()
     bg_green = im.getpixel(chord_green)
@@ -104,19 +105,20 @@ def play_song():
     bg_orange = im.getpixel(chord_orange)
     print(f"{datetime.now()}: Background colors", bg_green, bg_red, bg_yellow, bg_blue, bg_orange)
     
-    
     def watch_actions(action_queue):
         """
         Description: continuously gets frames and processes them into actions, inserting into a queue.
         """
         # Capture the frame to read specific pixels
-        nonlocal frame, held_buttons, holding, interval, bg_green, bg_red, bg_yellow, bg_blue, bg_orange
+        nonlocal frame, held_buttons, holding, interval, bg_green, bg_red, bg_yellow, bg_blue, bg_orange, scheduled_hold, scheduled_release
         print(f"Starting to watch actions...")
         idle_frames = 0
-        scheduled_hold = False
+        previous_pressed = []
+
         while idle_frames < 400:
             frame = frame + 1
             im = ImageGrab.grab()
+            frame_time = time()
 
             # Processing frame info
             if not holding:
@@ -125,7 +127,7 @@ def play_song():
                 color_yellow = im.getpixel(chord_yellow)
                 color_blue = im.getpixel(chord_blue)
                 color_orange = im.getpixel(chord_orange)
-                print(color_yellow, color_blue)
+                # print(color_yellow, color_blue)
                 buttons_to_press = []
                 should_hold = False
                 if color_green != bg_green:
@@ -144,25 +146,29 @@ def play_song():
                     buttons_to_press.append('l')
                     should_hold = should_hold or (im.getpixel(hold_orange) == orange_hold_color)
                 
-                if buttons_to_press == [] or len(buttons_to_press) == 5:
+                if buttons_to_press == [] or len(buttons_to_press) == 5 or buttons_to_press == previous_pressed:
                     idle_frames += 1
+                    previous_pressed = []
                     continue
                 if not scheduled_hold:
                     if should_hold:
                         idle_frames = 0
                         print(f"Adding action to queue: hold {buttons_to_press}")
                         scheduled_hold = True
-                        action_queue.put({'action': 'hold', 'buttons': buttons_to_press, 'frame': frame + interval})
+                        action_queue.put({'action': 'hold', 'buttons': buttons_to_press, 'when': frame_time + interval})
                     else:
                         idle_frames = 0
                         print(f"Adding action to queue: press {buttons_to_press}")
-                        action_queue.put({'action': 'press', 'buttons': buttons_to_press, 'frame': frame + interval})
+                        action_queue.put({'action': 'press', 'buttons': buttons_to_press, 'when': frame_time + interval})
+                    previous_pressed = buttons_to_press
             else:
                 color_green = im.getpixel(hold_green)
                 color_red = im.getpixel(hold_red)
                 color_yellow = im.getpixel(hold_yellow)
                 color_blue = im.getpixel(hold_blue)
                 color_orange = im.getpixel(hold_orange)
+                print("Checking for release:")
+                print(color_yellow, color_orange)
                 should_release = (
                     ('a' in held_buttons and color_green != green_hold_color) or
                     ('s' in held_buttons and color_red != red_hold_color) or
@@ -170,43 +176,49 @@ def play_song():
                     ('k' in held_buttons and color_blue != blue_hold_color) or 
                     ('l' in held_buttons and color_orange != orange_hold_color)
                 )
-                if should_release and not (action_queue[-1]['action'] == 'release'):
-                    action_queue.put({'action': 'release', 'buttons': None, 'frame': frame + interval})
+                print("Release?", should_release)
+                if should_release and not scheduled_release:
+                    print("Schedule release of held buttons")
+                    scheduled_release = True
+                    action_queue.put({'action': 'release', 'buttons': held_buttons, 'when': frame_time + interval})
         action_queue.put({'action': 'shutdown'})
-
 
     def execute_actions(action_queue):
         """
         Description: performs actions in the action queue.
         """
-        nonlocal holding, held_buttons, frame
+        nonlocal holding, held_buttons, scheduled_hold, scheduled_release
+        threshold = 0.08
         while True:
             last_action = action_queue.get()
+            current_time = time()
             if last_action['action'] == 'shutdown':
                 break
-            print(f"Got action! {last_action}, current frame: {frame}")
-            if last_action['frame'] == frame:
-                print(f"Executing action {last_action} at frame {frame}")
+            if abs(last_action['when'] - current_time) < threshold:
                 if last_action['action'] == 'press':
                     print(f"{datetime.now()}: Pressing", last_action['buttons'])
                     pg.press(last_action['buttons']) 
                 elif last_action['action'] == 'hold' and not holding:
                     holding = True
+                    scheduled_hold = False
                     held_buttons = last_action['buttons']
                     print(f"{datetime.now()}: Holding", held_buttons)
                     for button in held_buttons:
                         pg.keyDown(button)
                 elif last_action['action'] == 'release':
                     holding = False
+                    scheduled_release = False
                     print(f"{datetime.now()}: Releasing", held_buttons)
                     for button in held_buttons:
                         pg.keyUp(button)
                     held_buttons = []
                 else:
                     assert False, f"Unhandled action {last_action['action']}"
+            else:
+                action_queue.put(last_action)
     
     action_queue = queue.Queue()
-    action_queue.put({'action': None, 'frame': -1})
+    action_queue.put({'action': None, 'when': -1})
     watch_thread = threading.Thread(target=watch_actions, args=(action_queue,))
     exec_thread = threading.Thread(target=execute_actions, args=(action_queue,))
 
@@ -232,7 +244,7 @@ def main():
     start_game(song)
     print(f"{datetime.now()}: Starting game! Song: {song}")
     sleep(3) # Waiting 3 seconds to start song
-    for _ in range(10):
+    for _ in range(1):
         play_song()
         print(f"{datetime.now()}: Starting next song...")
         pg.click(next_button)
